9123a64af9bf37bba00d83126c917eb46e61d724
ianad <ian.dalton@ucf.edu>
2018-12-04 19:43:51 -0500

    day2/part1

---
+++ day2/part1.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+with open('input.txt') as f:
+    boxid_list = f.read().splitlines()
+
+double_boxids = 0
+triple_boxids = 0
+
+for boxid in boxid_list:
+    double_letters = len(list(filter(lambda x:x==2, Counter(boxid).values())))
+    triple_letters = len(list(filter(lambda x:x==3, Counter(boxid).values())))
+    if double_letters >= 1: double_boxids += 1;
+    if triple_letters >= 1: triple_boxids += 1;
+    print('boxid',boxid)
+    print('doubles',double_letters)
+    print('triples',triple_letters)
+
+print('total doubles',double_boxids)
+print('total triples',triple_boxids)
+
+checksum = double_boxids * triple_boxids
+print('checksum',checksum)
